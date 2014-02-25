9afdb18192f92c2b561a3eb4adda774871d0926a
Ben J. Ward <ben.ward@sainsbury-laboratory.ac.uk>
2014-02-25 03:45:39 +0000

    Generated recursive build function which goes through all nodes of the PhyloXML tree and builds the flattened tree comprised of many PhyloX nodes

--- PhyX.jl
+++ PhyX.jl
@@ -1,12 +1,21 @@
 using LightXML
 
 
-filepath = "/Users/wardb/Desktop/phylodev/phyxml2"
+filepath = "/Users/axolotlfan9250/Desktop/phylodev/phyxml2"
 treedoc = parse_file(filepath)
 
 phylogenies = get_elements_by_tagname(root(treedoc), "phylogeny")
 
 xmltree = phylogenies[1]
+
+type nodeTracker
+	nodeIndex::Int
+end
+
+type TestClade
+	name::ASCIIString
+	parent::Int
+end
 
 function phyXMLbuild(xmltree)
 	treestring = replace(string(xmltree), r"(\r|\n|\t)", "")
@@ -24,33 +33,35 @@ function phyXMLbuild(xmltree)
 	end
 	startclade != endclade ? println("Warning! There are unequal numbers of clade begins and clade ends in this tree") : println("Current tree has $startclade clade nodes")
 	# Ok the number of nodes has been established.
-	Clade = []			# Make an array to contain the Clade elements.
-	Track = []			# Make an array which tracks the parent of a Clade, to allow backtracking.
-	ChildrenDone =[] 	# Make an array to mark the number of children that have been moved over and completed.
-	Current = 1
-	Current = cladegen(Current, Track)
 
-
+	Clade = Array(TestClade, startclade)		# Make an array to contain the Clade elements.
+	BackTrack = zeros(Int, startclade)			# Make an array which tracks the parent of a Clade, to allow backtracking.
+	Current = nodeTracker(0)                    # Start the nodetracker type.
+	BackTrack[1] = 0
+	XML = get_elements_by_tagname(xmltree, "clade")[1]
+	recursiveBuild(XML, Clade, Current, 0)
 
 
 end
 
-function cladegen(current, track, childrendone, clade)
-	if current == 1
-		root = true
-	else
-		root = false
+
+
+function recursiveBuild(xmlclade, cladeArray, currentClade, parentClade::Int)
+	# Update the node tracker.
+	currentClade.nodeIndex += 1
+	current = currentClade.nodeIndex # initialize a loca variable called current, taken from the currentClade variable to keep as the variable to pass to furthur recursive calls as the parent index.
+	# Get name of clade element.
+	name = ""
+	# Get and process all additional data.... TODO
+	children = get_elements_by_tagname(xmlclade, "clade")
+	# Build the clade element.
+	cladeArray[currentClade.nodeIndex] = TestClade(name, parentClade)
+	for i in children
+		recursiveBuild(i, cladeArray, currentClade, current)
 	end
-
-	clade[current] = 
-
 end
 
-type TestClade
-	name::ASCIIString
-	parent::Int
-	children::Array{Int}
-end
+
 
 
 
